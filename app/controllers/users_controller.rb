cb9a00039d4f663bf9d70fbd04a0de41c4ba2b8b
max anselmo <itmanselmo@gmail.com>
2021-12-13 16:40:38 -0500

    initial set up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+    skip_before_action :authorize, only: [:create]
+    
+    def create
+        user = User.create(user_params)   
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+
+        if user
+            render json: user, status: :ok
+        else
+            render json: {error: ["Not found"]}, status: :not_found
+        end
+
+    end
+
+      private
+
+    def user_params
+        params.permit(
+            :username,
+            :password
+        )
+    end
+
+end
