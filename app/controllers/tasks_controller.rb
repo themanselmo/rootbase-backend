1a112331b0a374fa3eed7a25bcc4be0378728e7f
max anselmo <itmanselmo@gmail.com>
2021-12-17 15:20:26 -0500

    added controller methods so employees can now update the status of tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,37 @@
 class TasksController < ApplicationController
 
+    def create 
+        task = Task.create(task_params)
+
+        if task
+            render json: task, status: :created
+        else
+            render json: {error: [task.errors]}, status: :unprocessable_entity
+        end
+
+    end
+
+    def update 
+        task = Task.find_by(id: params[:id])
+
+        if task
+            task.update(task_params)
+            render json: task, status: :ok
+        else
+            render json: {erros: [task.errors]}
+        end
+    end
+
+
+    private
+
+    def task_params 
+        params.permit(
+            :name,
+            :status,
+            :due_date
+        )
+
+    end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
     belongs_to :organization
+    has_many :employee_tasks
+    has_many :garden_tasks
     has_many :employees, through: :employee_tasks
     has_many :gardens, through: :garden_tasks
 end
