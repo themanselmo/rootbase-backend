dbcbfd2464888296eec85abedc57248ef0ba59b8
max anselmo <itmanselmo@gmail.com>
2021-12-20 15:52:11 -0500

    added controller method so users can create garden task associations

--- app/controllers/garden_tasks_controller.rb
+++ app/controllers/garden_tasks_controller.rb
@@ -1,2 +1,23 @@
 class GardenTasksController < ApplicationController
+
+    def create 
+        garden_task = GardenTask.create(garden_task_params)
+
+        if garden_task 
+            render json: garden_task, status: :created
+        else
+            render json: { error: garden_task.errors}, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+
+    def garden_task_params 
+        params.permit(
+            :task_id,
+            :garden_id
+        )
+
+    end
 end
--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -2,4 +2,5 @@ class OrganizationSerializer < ActiveModel::Serializer
   attributes :id, :name
   has_many :employees
   has_many :tasks
+  has_many :gardens
 end
