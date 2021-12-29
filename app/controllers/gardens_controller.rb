2f8aa0c43105e141534305cbd0210bb32da3a11c
max anselmo <itmanselmo@gmail.com>
2021-12-29 12:44:58 -0500

    created controller method for creating gardens

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,23 @@
 class GardensController < ApplicationController
+
+
+    def create 
+        garden = Garden.new(garden_params)
+        garden[:organization_id] = session[:user_id]
+
+        garden.save
+        if garden
+            render json: garden, status: :created
+        else
+            render json: {error: [garden.errors]}, status: :unprocessable_entity
+        end
+
+    end
+
+
+    private
+
+    def garden_params
+        params.permit(:name)
+    end
 end
